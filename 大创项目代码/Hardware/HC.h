2f5308a8efc061d09eb9dbfef996a8028b2b7ffe
田有桃 <2452986631@qq.com>
2024-11-02 16:08:53 +0800

    clear

--- 大创项目代码/Hardware/HC.h
+++
@@ -1,8 +0,0 @@
-#ifndef __HC_H
-#define __HC_H
-
-void Timer_SRD_Init(u16 arr,u16 psc);
-void TIM3_IRQHandler(void);
-
-#endif
-
--- 大创项目代码/Hardware/timer.h
+++
@@ -1,8 +0,0 @@
-#ifndef _TIMER_H
-#define _TIMER_H
-#include "sys.h"
-
-void Timer_SRD_Init(u16 arr,u16 psc);
-void TIM2_Int_Init(u16 arr,u16 psc);
-
-#endif
